30a40bf130cab72137f1afe0e1801151da75101e
julienasp <julien.aspirot@usherbrooke.ca>
2016-06-24 17:40:42 -0400

    ya du progressssssssssssssss

--- client.py
+++ client.py
@@ -14,36 +14,24 @@ print('Client: connecting to %s port %s' % server_address)
 #connect to the server socket with TCP
 sock.connect(server_address)
 
+#We receive the key
+data = sock.recv(2048)
+
 #RSA Settings
-key = RSA.generate(1024) #generate pub and priv key
-
+key = RSA.importKey(data)
 publickey = key.publickey() # pub key export for exchange
-print('Client: publickey is %s' % key.exportKey())
 
-encrypted = publickey.encrypt("secret msg", 32)
-
-print('encrypted message:', encrypted) #ciphertext
-
-decrypted = key.decrypt(ast.literal_eval(str(encrypted)))
-
-print('decrypted', decrypted)
-
-
+print('Client: the server publickey is %s' % key.exportKey())
 try:
     # Send data
-    message = 'This is the message.  It will be repeated.'
-    print('Client: sending "%s"' % message)
-    sock.sendall(message)
+    message = input('Please type the message you want to send to the server: ')
+    print('Client: message is "%s"' % message)
 
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(message)
+    #Encryption
+    encrypted = publickey.encrypt(message)
+    print('Client: will send the following encrypted message "%s"' % encrypted)
 
-    while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-        print('Client: received "%s"' % data)
-
+    sock.sendall(encrypted)
 finally:
     print('Client: closing socket')
     sock.close()
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 import socket
-
+import Crypto
+from Crypto.PublicKey import RSA
+#from crypto import Random
+import ast
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -13,6 +16,12 @@ sock.bind(server_address)
 # Listen for incoming connections - MAX 1 connection
 sock.listen(1)
 
+#RSA Settings
+key = RSA.generate(1024) #generate pub and priv key
+
+publickey = key.publickey() # pub key export for exchange
+print('Server: publickey is %s' % key.exportKey())
+
 while True:
     # Wait for a connection
     print 'Server: waiting for a connection'
@@ -21,13 +30,18 @@ while True:
     try:
         print 'Server: connection from', client_address
 
+        #We send our public key
+        print 'Server: sending publicKey to', client_address
+        connection.send(key.exportKey())
+
         # Receive the data in small chunks and retransmit it
         while True:
-            data = connection.recv(16)
-            print 'Server: received "%s"' % data
-            if data:
-                print 'Server: sending data back to the client'
-                connection.sendall(data)
+            encryptedData = connection.recv(1024)
+            print 'Server: received the encrypted message: "%s"' % encryptedData
+            if encryptedData:
+                print 'Server: is decrypting the message...'
+                decrypted = key.decrypt(ast.literal_eval(str(encryptedData)))
+                print'Server: decrypted data is: %s' % decrypted
             else:
                 print 'Server: no more data from', client_address
                 break
