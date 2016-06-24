d5ed68371013b9f1e25d566f52bd107b31ae72f1
julienasp <julien.aspirot@usherbrooke.ca>
2016-06-24 12:28:22 -0400

    :dart:

--- server.py
+++ server.py
@@ -1,12 +1,11 @@
 import socket
-import sys
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #Create a tuple server_address
-server_address = ('localhost', 10000)
-print (sys.stderr, 'starting up on %s port %s' % server_address)
+server_address = ('localhost', 13337)
+print ('starting up on %s port %s' % server_address)
 
 # Bind the socket to the port
 sock.bind(server_address)
@@ -16,21 +15,21 @@ sock.listen(1)
 
 while True:
     # Wait for a connection
-    print (sys.stderr, 'waiting for a connection')
+    print ('waiting for a connection')
     connection, client_address = sock.accept()
 
     try:
-        print ( sys.stderr, 'connection from', client_address)
+        print ('connection from', client_address)
 
         # Receive the data in small chunks and retransmit it
         while True:
             data = connection.recv(16)
-            print ( sys.stderr, 'received "%s"' % data)
+            print ('received "%s"' % data)
             if data:
-                print ( sys.stderr, 'sending data back to the client')
+                print ('sending data back to the client')
                 connection.sendall(data)
             else:
-                print ( sys.stderr, 'no more data from', client_address)
+                print ('no more data from', client_address)
                 break
 
     finally:
