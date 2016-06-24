18f7dd3a1ee197956aacc0e7173e3e5486443111
julienasp <julien.aspirot@usherbrooke.ca>
2016-06-24 14:13:31 -0400

    update

--- client.py
+++ client.py
@@ -1,9 +1,65 @@
 import socket
+import Crypto
+from Crypto.PublicKey import RSA
+from Crypto import Random
 
-s = socket.socket()
-host = socket.gethostname()
-port = 12345
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.connect((host, port))
-print s.recv(1024)
-s.close
+#Create a tuple server_address
+server_address = ('localhost', 58432)
+print('Client: connecting to %s port %s' % server_address)
+
+#connect to the server socket with TCP
+sock.connect(server_address)
+
+#RSA Settings
+
+import ast
+
+random_generator = Random.new().read
+key = RSA.generate(1024, random_generator) #generate pub and priv key
+
+publickey = key.publickey() # pub key export for exchange
+
+encrypted = publickey.encrypt('encrypt this message', 32)
+#message to encrypt is in the above line 'encrypt this message'
+
+print 'encrypted message:', encrypted #ciphertext
+f = open ('encryption.txt', 'w')
+f.write(str(encrypted)) #write ciphertext to file
+f.close()
+
+#decrypted code below
+
+f = open('encryption.txt', 'r')
+message = f.read()
+
+
+decrypted = key.decrypt(ast.literal_eval(str(encrypted)))
+
+print 'decrypted', decrypted
+
+f = open ('encryption.txt', 'w')
+f.write(str(message))
+f.write(str(decrypted))
+f.close()
+
+try:
+    # Send data
+    message = 'This is the message.  It will be repeated.'
+    print('Client: sending "%s"' % message)
+    sock.sendall(message)
+
+    # Look for the response
+    amount_received = 0
+    amount_expected = len(message)
+
+    while amount_received < amount_expected:
+        data = sock.recv(16)
+        amount_received += len(data)
+        print('Client: received "%s"' % data)
+
+finally:
+    print('Client: closing socket')
+    sock.close()
